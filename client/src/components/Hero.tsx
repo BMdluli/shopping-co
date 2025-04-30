5aee730579b2b0f56f9d3f059de80361e93959e2
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-04-30 09:18:55 +0200

    add: added hero section

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,34 @@
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 md:flex-row md:justify-between md:h-[663px]">
+        <div className="flex flex-col gap-4 flex-1 md:justify-center md:gap-10">
+          <h1 className="text-4xl font-black uppercase md:text-6xl">
+            find clothes <br className="hidden md:block" /> that matches{" "}
+            <br className="hidden md:block" /> your style
+          </h1>
+          <p className="text-xs text-gray-500 font-light md:text-base">
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </p>
+
+          <button className="h-[52px] rounded-full bg-black text-white w-full md:max-w-[210px]">
+            Shop Now
+          </button>
+        </div>
+
+        <div className="flex-1">
+          <img
+            className="object-cover h-full w-full"
+            src="/hero.jpg"
+            alt="hero"
+          />
+        </div>
+      </div>
+      <div className="h-20 bg-black"></div>
+    </div>
+  );
+};
+
+export default Hero;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div>
+    <div className="max-w-[1240px] mx-auto">
       <Header />
+      <Hero />
     </div>
   );
 };
